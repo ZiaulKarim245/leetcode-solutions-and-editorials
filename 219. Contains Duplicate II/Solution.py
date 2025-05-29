044839bb6be5ff3cb8478e1c035dffd2cbe822c8
Ziaul Karim <ziaulkari278@gmail.com>
2025-05-29 15:59:48 +0600

    solution and editorial

--- 219. Contains Duplicate II/Solution.py
+++ 219. Contains Duplicate II/Solution.py
@@ -1,12 +1,20 @@
 class Solution:
     def containsNearbyDuplicate(self,nums,k):
         # Solution 1
-        con_dup = {}
+        '''con_dup = {}
         for i,num in enumerate(nums):
             if num in con_dup and i - con_dup[num] <= k:
                 return True
             con_dup[num] = i
+        return False'''
+        # Solution 2
+        if len(nums) == len(set(nums)):
+            return False
+        for i in range(len(nums)):
+            if nums[i] in nums[i+1:i+1+k]:
+                return True
         return False
+        # Solution 3
         
 
 
